c4c212ef0ab10f36e5d0a870e972c355745339b6
Steve Knight <steve@hackinghat.com>
2014-09-14 16:37:23 +0100

    R is weird

--- R Programming/Assignment 1/corr.R
+++ R Programming/Assignment 1/corr.R
@@ -1,11 +1,14 @@
+source("complete.R")
 corr <- function(directory, threshold = 0) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+  all_monitors<-vapply(dir(directory), function(x) { as.numeric(substr(x, 1, 3)) }, numeric(1))
+  valid<-complete(directory, all_monitors)
+  id<-valid$id[valid$nobs>=threshold]
   
-  ## 'threshold' is ac numeric vector of length 1 indicating the
-  ## number of completely observed observations (on all
-  ## variables) required to compute the correlation between
-  ## nitrate and sulfate; the default is 0
-  
-  ## Return a numeric vector of correlations
+  makepath <- function(x) { 
+    sprintf("%s/%03d.csv", directory, x) 
+  }
+  monitors<-lapply(vapply(id, makepath, character(1)), read.csv)
+  data <- lapply(monitors, function(s) { subset(s, !is.na(sulfate) & !is.na(nitrate)) })
+  reading_matrix <-lapply(data, function(x) { x[c("sulfate", "nitrate")] } )
+  sapply(reading_matrix, function(x) { cor(x$sulfate, x$nitrate) })
 }
